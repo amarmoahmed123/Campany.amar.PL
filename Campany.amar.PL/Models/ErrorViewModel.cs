cc2bdc288bd45a592c42df634bb891d7b7ec3a47
3MAR <3MAR@3MAR>
2025-03-20 01:44:03 +0200

    Add project files.

---
+++ Campany.amar.PL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Campany.amar.PL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
