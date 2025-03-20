1be2b9299a2214f1e57aa361ec8b790c4cd3992d
3MAR <3MAR@3MAR>
2025-03-20 02:18:11 +0200

    Implmentation Of DAL

---
+++ Campany.amar.DAL/Data/Context/CampanyDbContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Campany.amar.DAL.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Campany.amar.DAL.Data.Context
+{
+    public class CampanyDbContext : DbContext
+    {
+        public CampanyDbContext() : base()
+        { 
+        
+         
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server= 3MAR\\SQLEXPRESS;Database= Campanyamar; Trusted_Connection = True; TrustServerCretificate= True");
+
+
+        }
+        public DbSet<Department> Departments { get; set; }
+    }
+}
